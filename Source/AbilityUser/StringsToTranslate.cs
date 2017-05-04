aae8fcf9c3770dc5090424c473cf529beedd6bc3
jecrell <matt.walls31@gmail.com>
2017-05-04 21:17:29 +0900

    A17 Update (Minor fix)

--- Source/AbilityUser/StringsToTranslate.cs
+++ Source/AbilityUser/StringsToTranslate.cs
@@ -6,25 +6,25 @@ using System.Text;
 namespace AbilityUser
 {
     public static class StringsToTranslate
-    {
-        public static readonly string AU_AoEProperties = "";
-        public static readonly string AU_TargetClass = "";
-        public static readonly string AU_AoECharacters = "";
-        public static readonly string AU_AoEFriendlyFire = "";
-        public static readonly string AU_AoEMaxTargets = "";
-        public static readonly string AU_AoEStartsFromCaster = "";
-        public static readonly string AU_Cooldown = "";
-        public static readonly string AU_Type = "";
-        public static readonly string AU_TargetAoE = "";
-        public static readonly string AU_TargetSelf = "";
-        public static readonly string AU_TargetThing = "";
-        public static readonly string AU_TargetLocation = "";
-        public static readonly string AU_Extra = "";
-        public static readonly string AU_MentalStateChance = "";
-        public static readonly string AU_EffectChance = "";
-        public static readonly string AU_BurstShotCount = "";
-        public static readonly string AU_CastSuccess = "";
-        public static readonly string AU_CastFailure = "";
-        public static readonly string AU_DISABLED = "";
+    { //
+        public static readonly string AU_AoEProperties = "Area of Effect Properties";
+        public static readonly string AU_TargetClass = "Targets: ";
+        public static readonly string AU_AoECharacters = "Characters";
+        public static readonly string AU_AoEFriendlyFire = "Friendly Fire: ";
+        public static readonly string AU_AoEMaxTargets = "Max Targets: ";
+        public static readonly string AU_AoEStartsFromCaster = "Starts from caster: ";
+        public static readonly string AU_Cooldown = "Cooldown: ";
+        public static readonly string AU_Type = "Type: ";
+        public static readonly string AU_TargetAoE = "Area of Effect";
+        public static readonly string AU_TargetSelf = "Targets Self";
+        public static readonly string AU_TargetThing = "Targets Other";
+        public static readonly string AU_TargetLocation = "Targets Location";
+        public static readonly string AU_Extra = "Extra";
+        public static readonly string AU_MentalStateChance = "Mental State Chance";
+        public static readonly string AU_EffectChance = "Effect Chance";
+        public static readonly string AU_BurstShotCount = "Burst Count:";
+        public static readonly string AU_CastSuccess = "Cast Success";
+        public static readonly string AU_CastFailure = "Cast Failed";
+        public static readonly string AU_DISABLED = "DISABLED";
     }
 }
